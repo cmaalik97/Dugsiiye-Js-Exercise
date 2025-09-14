f01170db892990c4e67d6033edc7f0f24eabbe0f
cmaalik97 <cmaalik97@gmail.com>
2025-09-14 10:01:03 -0700

    Exercise 2

---
+++ Exercise2/main.js
@@ -0,0 +1,14 @@
+console.log('This two values We Do compare!!')
+let x=15;
+let y="15";
+console.log("x:",x)
+console.log("y:",y)
+console.log("Comparison operaters: ==, != ,< ,>, and so on")
+console.log("use ==:",x==y)
+console.log("use !=:",x!=y)
+console.log("use ===:",x===y)
+console.log("use !==:",x!==y)
+console.log("use >:",x>y)
+console.log("use <:",x<y)
+console.log("use >=:",x>=y)
+console.log("use <=:",x<=y)

16de4b9b77e0c35106a3510704c9db7e5ee6c4de
cmaalik97 <cmaalik97@gmail.com>
2025-09-14 20:38:56 -0700

    Exercise 3 Answers

---
+++ Exercise3/README.md
@@ -0,0 +1,2 @@
+# Exercise3
+## Logical Operator
---
+++ Exercise3/main.js
@@ -0,0 +1,6 @@
+console.log("Logical Operaters: ")
+console.log("We use two logical isSunny and isWeakend ")
+console.log("to check if you can go to beach ")
+let isSunny=true;
+let isWeakend=true;
+console.log(isSunny && isWeakend)

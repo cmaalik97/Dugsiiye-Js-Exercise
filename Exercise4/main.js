494612c2c0dfd4eeba8fe26804f0de6b4967b1dd
cmaalik97 <cmaalik97@gmail.com>
2025-09-16 11:06:53 -0700

    Exercise 4

---
+++ Exercise4/main.js
@@ -0,0 +1,24 @@
+//Functions
+console.log("Functions Question")
+console.log("Basic type Functions ")
+console.log("I use two parameters and retun their sum ")
+ function Calc(x,y){
+    return x+y;
+ }
+ //call function
+ console.log(Calc(10,20))
+ console.log(Calc(20,30))
+ console.log(Calc(100,30))
+
+//expressions function it is a function creat in a varaible 
+//i mean the function will a value in a variable that is called Function Expressions 
+console.log(" ")
+console.log(" ")
+console.log("Function Quesions ")
+console.log("Function Expression")
+const addfun=function(x,y){
+    return x+y;
+}
+console.log(addfun(20,30))
+console.log(addfun(50,0))
+console.log(addfun(10,90))

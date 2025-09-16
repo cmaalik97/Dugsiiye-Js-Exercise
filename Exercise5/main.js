fd96c1a2d60559dcde96907242901bbf9b270b5a
cmaalik97 <cmaalik97@gmail.com>
2025-09-16 11:17:17 -0700

    Exercise 5

---
+++ Exercise5/main.js
@@ -0,0 +1,12 @@
+//Arrow Functions : is a short that you can use
+// arrows => instead of function keyword
+console.log("Arrow Function")
+console.log("use two parameters to add")
+// declear function
+const addfun=(x,y)=>{
+    return x+y;
+}
+ // call the function
+ console.log(addfun(10,20))
+ console.log(addfun(30,20))
+ console.log(addfun(11,23))

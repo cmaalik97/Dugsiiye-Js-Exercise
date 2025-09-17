75404fda2137aef27b46712ff484015f50b561ed
cmaalik97 <cmaalik97@gmail.com>
2025-09-17 12:01:12 -0700

    Exercise 8

---
+++ Exercise8/main.js
@@ -0,0 +1,12 @@
+console.log("Objects:  ")
+//objects talks about the info or a data of the thing
+// i can also say a key and value 
+let car={
+    make:"Toyota",
+    model:"Coralla",
+    year:2020,
+}
+console.log(car)
+console.log(car.make)
+console.log(car.model)
+console.log(car.year)

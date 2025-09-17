05d4c69cb63ac6dfb3c22bd42c3e83434dce2610
cmaalik97 <cmaalik97@gmail.com>
2025-09-17 11:45:00 -0700

    Exercise 7

---
+++ Exercise7/main.js
@@ -0,0 +1,21 @@
+//Array methods
+//push and unshift is used to add *push at the end *unshift at beggining
+//pop and shift is used to remove *pop at the end * shift at the beggining
+console.log("Arrays Method: ")
+let colors=["Red","Blue", "Orange", "Green"]
+console.log("before the pop")
+console.log(colors)
+colors.pop()
+console.log("after the pop")
+console.log(colors)
+
+console.log("after the shift")
+colors.shift()
+console.log(colors)
+//unshift and push
+
+console.log("")
+console.log("")
+console.log("after unshift")
+colors.unshift("Black")
+console.log(colors)

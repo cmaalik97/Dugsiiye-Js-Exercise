408d9d35531308f320008baac3d071edf0e56d14
cmaalik97 <cmaalik97@gmail.com>
2025-09-21 20:57:41 -0700

    Exercise 10

---
+++ Exercise10/README.md
@@ -0,0 +1,3 @@
+# Exercise 10
+## Array Objects
+### Create an array named Library
---
+++ Exercise10/main.js
@@ -0,0 +1,28 @@
+//Array object :that means we can store names with properties
+let Library=[
+    {
+        title:"Aqoon Guud",
+        author:"Ali Jimale Ahmed",
+        year:2005
+    },
+    {
+        title:"Sheekooyin Soomaliyed",
+        author:"Hussein Sheikh Ahmed kaddare",
+        year:1987
+    },
+    {
+        title:"Nolol iyo Halgan",
+        author:"Mohamed Hadraawi",
+        year:2010
+    }
+]
+
+//so now in print or get the output
+//As in exercise print the title of the first
+//second print the author of the second book
+//third print the year of the third book, 
+console.log(Library)
+// so now we do
+console.log(Library[0].title)
+console.log(Library[1].author)
+console.log(Library[2].year)

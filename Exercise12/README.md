fcc6b690c764289dd806e3caa6c9d9e6c9b7ad89
cmaalik97 <cmaalik97@gmail.com>
2025-09-23 20:49:26 -0700

    Exercise 12

---
+++ Exercise12/README.md
@@ -0,0 +1,3 @@
+# Exercise 12
+## Loops Basic 
+### for loop with Array
---
+++ Exercise12/main.js
@@ -0,0 +1,6 @@
+//For loop with Array 
+console.log("FRUITS: ")
+const fruits=["Apple ","Bannna","Charry", "Mango"]
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i])
+}

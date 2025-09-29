1dc718eeda2a5f7af3880f0cfeee32252c87fb55
cmaalik97 <cmaalik97@gmail.com>
2025-09-28 22:41:27 -0700

    Exercise 16

---
+++ Exercise16/main.js
@@ -0,0 +1,9 @@
+//if-else statements
+// condition and what you to do for if the condition is true or false
+let score=50;
+if(score >=50){
+    console.log("You Passed!!")
+}
+else{
+    console.log("You failed!!")
+};

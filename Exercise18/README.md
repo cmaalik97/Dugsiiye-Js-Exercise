c0aac477853cf29f43ae92ab7afc37fae9bd77b9
cmaalik97 <cmaalik97@gmail.com>
2025-10-05 12:20:18 -0700

    Exercise 18

---
+++ Exercise18/README.md
@@ -0,0 +1,2 @@
+# Exercise 18
+## Ternary Operator
---
+++ Exercise18/main.js
@@ -0,0 +1,10 @@
+//it's Short way of if-else  Statements
+let grade=60;
+console.log(grade>=60?'pass':'fail');
+
+
+
+// one single step of
+let age=20;
+const message=age>15 &&"You're Adult";
+console.log(message)

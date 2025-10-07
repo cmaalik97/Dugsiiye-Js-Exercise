20afe477018ca12c5ca2631d12db0d167f58a4fc
cmaalik97 <cmaalik97@gmail.com>
2025-10-07 06:57:22 -0700

    Exercise 20

---
+++ Exercise20/main.js
@@ -0,0 +1,9 @@
+//map() is one of high order arrays methods
+// is used to create new array populated
+//with the result of calling a provided function
+
+const fruits=["apple","banana", "cherry"]
+const newArray=fruits.map(fruit=>fruit.length)
+console.log(newArray)
+
+

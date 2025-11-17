db67755fcdbce64926bbcb1161a59b22f6a5f91f
cmaalik97 <cmaalik97@gmail.com>
2025-11-17 09:23:44 -0800

    Exercise 35

--- Exercise35/main.js
+++ Exercise35/main.js
@@ -5,17 +5,17 @@ function changeImg(){
     const imgs=document.querySelector("#img");
     const newImg=prompt("Enter the url of img ?")
     const border=prompt("Enter the border color ?")
-    const width=prompt("Enter the width of the img (as pixels)")
-    const height=prompt("Enter the height of the img (as pixels)")
-    const radius=prompt("Enter the border radius size (as pixels)")
-    const paddingimg=prompt("Enter the padding size (as pixels)")
+    const width=prompt("Enter the width of the img ( pixels)")
+    const height=prompt("Enter the height of the img ( pixels)")
+    const radius=prompt("Enter the border radius size ( pixels)")
+    const paddingimg=prompt("Enter the padding size ( pixels)")
     const backgcolor=prompt("Enter background color?")
 
     imgs.setAttribute("src",newImg)
     imgs.style.border=`2px solid ${border}`;
     imgs.style.width=`${width}px`;
     imgs.style.height=`${height}px`;
-    imgs.style.borderRadius=radius;
+    imgs.style.borderRadius=`${radius}px`;
     imgs.style.padding=paddingimg;
     imgs.style.backgroundColor=backgcolor;
   
